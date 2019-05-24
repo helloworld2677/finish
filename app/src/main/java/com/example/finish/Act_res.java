9f73856108adea1c87cc3e015743970c2027a71f
mehdi <mehdi.masoomi2677@gmail.com>
2019-05-24 01:39:39 -0700

    commit2

---
+++ app/src/main/java/com/example/finish/Act_res.java
@@ -0,0 +1,13 @@
+package com.example.finish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Act_res extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_res);
+    }
+}
